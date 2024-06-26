7bea8f69ee8e1e30464705396820516fa2aec9a6
Lorena Tuhay <lorena.tuhay@gmail.com>
2024-06-25 22:11:45 -0300

    SP Borrowed Books Report

---
+++ Complex Queries and Procedures/SP Borrowed Books Report.sql
@@ -0,0 +1,18 @@
+DROP PROCEDURE IF EXISTS sp_BorrowedBooksReport;
+GO
+
+CREATE PROCEDURE sp_BorrowedBooksReport
+    @StartDate DATE,
+    @EndDate DATE
+AS
+BEGIN
+	SELECT b.BookID, b.Title, bo.FirstName, bo.LastName, l.DateBorrowed
+	FROM Books b
+	JOIN Loans l
+	ON b.BookID = l.BookID
+	JOIN Borrowers bo
+	ON bo.BorrowerID = l.BorrowerID
+	WHERE l.DateBorrowed BETWEEN @StartDate AND @EndDate
+	ORDER BY l.DateBorrowed;
+END
+GO
