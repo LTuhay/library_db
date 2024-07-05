541dc373b51a1fbcc3470b681edc0b814797eb98
Lorena Tuhay <lorena.tuhay@gmail.com>
2024-07-05 12:13:46 -0300

    Refactored Genre Preference by Age query to use HAVING and CTE

--- Complex Queries and Procedures/Genre Preference by Age.sql
+++ Complex Queries and Procedures/Genre Preference by Age.sql
@@ -1,26 +1,32 @@
+WITH AgeGroups AS (
+    SELECT 
+        br.BorrowerID,
+        b.BookID,
+        b.Genre,
+        CASE 
+            WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 0 AND 10 THEN '0-10'
+            WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 11 AND 20 THEN '11-20'
+            WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 21 AND 30 THEN '21-30'
+            ELSE '31+'
+        END AS AgeGroup
+    FROM 
+        Borrowers br
+    JOIN 
+        Loans l ON br.BorrowerID = l.BorrowerID
+    JOIN 
+        Books b ON l.BookID = b.BookID
+)
 SELECT 
-    CASE 
-        WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 0 AND 10 THEN '0-10'
-        WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 11 AND 20 THEN '11-20'
-        WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 21 AND 30 THEN '21-30'
-        ELSE '31+'
-    END AS AgeGroup,
-    b.Genre, 
-    COUNT(l.LoanID) AS GenreCount
+    AgeGroup,
+    Genre, 
+    COUNT(*) AS GenreCount
 FROM 
-    Loans l
-JOIN 
-    Borrowers br ON l.BorrowerID = br.BorrowerID
-JOIN 
-    Books b ON l.BookID = b.BookID
+    AgeGroups
 GROUP BY 
-    CASE 
-        WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 0 AND 10 THEN '0-10'
-        WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 11 AND 20 THEN '11-20'
-        WHEN DATEDIFF(YEAR, br.DateOfBirth, GETDATE()) BETWEEN 21 AND 30 THEN '21-30'
-        ELSE '31+'
-    END, 
-    b.Genre
+    AgeGroup, 
+    Genre
+HAVING 
+    COUNT(*) > 0
 ORDER BY 
     AgeGroup, 
     GenreCount DESC;
