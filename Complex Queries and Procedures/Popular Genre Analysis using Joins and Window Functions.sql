85a40a399afba6b792acb6a3159f2df5e4936277
Lorena Tuhay <lorena.tuhay@gmail.com>
2024-07-05 11:45:10 -0300

    Remove year from Popular Genre Analysis query

--- Complex Queries and Procedures/Popular Genre Analysis using Joins and Window Functions.sql
+++ Complex Queries and Procedures/Popular Genre Analysis using Joins and Window Functions.sql
@@ -1,5 +1,4 @@
 DECLARE @Month INT = 6; 
-DECLARE @Year INT = 2023;
 SELECT 
     b.Genre, 
     COUNT(l.LoanID) AS LoansCount,
@@ -10,7 +9,6 @@ JOIN
     Loans l ON b.BookID = l.BookID
 WHERE 
     MONTH(l.DateBorrowed) = @Month
-    AND YEAR(l.DateBorrowed) = @Year
 GROUP BY 
     b.Genre
 ORDER BY 
